f969032d486f8f7aecddedd1e91e220554299808
Ji Yim <gsooyim@gmail.com>
2018-04-11 02:12:11 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if 40 < speed < 60
+  if 60 < speed < 40
     true
   else
     false
@@ -9,7 +9,7 @@ end
 
 
 def not_safe?(speed)
-	40 < speed < 60 ? true : false
+	60 < speed < 40 ? true : false
 end
 	
 
